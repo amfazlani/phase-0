90b37e7bbcea3931e48153a3231e5645fc494a5a
Amaar Fazlani <fazlaniamaar@gmail.com>
2015-10-26 11:18:50 -0500

    guess file

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,18 @@
+# Good Guess
+
+# I worked on this challenge [by myself, with: ].
+
+
+#define a method
+#returns true if number is 42
+#returns false if number is anything else
+
+# Your Solution Below
+
+def good_guess?(my_number)
+  if my_number = 42
+    return true
+  else
+    return false
+  end
+end

b613fac3d9d287f85b89180d93b3ee90e1c01131
Amaar Fazlani <fazlaniamaar@gmail.com>
2015-11-16 02:23:40 -0600

    challenge 1

---
+++ week-7/eloquent.js
@@ -0,0 +1,53 @@
+// Eloquent JavaScript
+
+// Run this file in your terminal using `node my_solution.js`. Make sure it works before moving on!
+
+// Program Structure
+// Write your own variable and do something to it.
+
+var my_variable = 44
+my_variable / 11
+console.log(my_variable)
+
+// Prompt
+prompt("What is your favorite food");
+console.log("Hey! That's my favorite too!")
+
+// Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
+
+var hash = "#";
+while (hash.length <= 7) {
+console.log(hash)
+hash += "#";
+}
+
+// Functions
+
+// Complete the `minimum` exercise.
+
+var min = function(a,b) {
+  if (a > b)
+    return b;
+  else
+    return a;
+
+}
+
+// Data Structures: Objects and Arrays
+// Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+
+var me = {
+  name: "Amaar",
+  age: 23,
+  favoriteFood: ["pizza","burger","fries"],
+  quirk: "Does not know how to swim.",
+
+};
+
+
+
+
+
+
+
+
